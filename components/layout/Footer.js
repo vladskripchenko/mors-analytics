6d996edee19e32ca1e7c84fb5253e234530773f5
Vlad Skripchenko <73851441+vladskripchenko@users.noreply.github.com>
2022-04-23 15:59:29 +0300

    Update Footer.js

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -22,7 +22,7 @@ export default function Footer() {
             values={{
               name: (
                 <Link href="https://mors.agency">
-                  Mors
+                  <b>Mors</b>
                 </Link>
               ),
             }}
